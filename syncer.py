6c9738b421350cacdef19b4b6b13ff6a6e125163
Lorenzo Biosa <lorenzo.biosa@accenture.com>
2025-07-28 17:20:25 +0200

    Updated log

--- syncer.py
+++ syncer.py
@@ -4,13 +4,13 @@ from datetime import datetime
 import os
 
 def run_rsync(line):
-    source, dest = line.split(',')
+    source, dest = line.split(',', 1)
 
     command = [
         'rsync', '-azHAXv', '--delete', source, dest
     ]
 
-    with open('rsync.log', 'a') as log_file:
+    with open('rsync.log', 'a', encoding='utf-8', errors='surrogateescape') as log_file:
         log_file.write(f'{datetime.now()} - Start: {source} -> {dest}\n')
         subprocess.run(command, stdout=log_file, stderr=log_file)
         log_file.write(f'{datetime.now()} - End: {source} -> {dest}\n')
@@ -19,11 +19,11 @@ def main():
     with open('rsync.log', 'a') as log_file:
         log_file.write(f'{datetime.now()} - Start of sync process\n')
 
-    with open('files.txt', 'r') as file:
+    with open('files.txt', 'r', encoding='utf-8', errors='surrogateescape') as file:
         lines = file.readlines()
 
     with ProcessPoolExecutor(max_workers=os.cpu_count()*4) as executor:
-        futures = [executor.submit(run_rsync, line.strip()) for line in lines]
+        futures = [executor.submit(run_rsync, line.strip('\n\r\t\f\v')) for line in lines]
 
         for future in as_completed(futures):
             try:
